e204e439b9c556cc244d26ece64f4c60c30b5643
Valko Martin <matinko.valko@gmail.com>
2023-11-06 21:27:25 +0100

    Init

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,19 @@
+class Game {
+
+    var board = Array(10) { Array<Any?> (10) {null} }
+
+    init {
+        board =
+    }
+
+
+
+   fun printBoard() {
+       for (i in 0 .. 9) {
+           for (j in 0 .. 9) {
+               print(board[i][j])
+           }
+           println()
+       }
+   }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val game = Game();
+
+    game.createBoard()
+    game.printBoard()
+
+}
