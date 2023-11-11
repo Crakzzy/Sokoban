4f3b5d669afdf7976e3da11e59206e9e05d971cf
Valko Martin <matinko.valko@gmail.com>
2023-11-11 21:42:38 +0100

    Added welcome message and game instructions to main function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,17 @@
+fun welcome() {
+    println("Made by Martin Valko (https://github.com/Crakzzy)")
+    println("Welcome to Sokoban game!")
+    println("Inputs are: ")
+    println("W - UP")
+    println("A - LEFT")
+    println("S - RIGHT")
+    println("D - DOWN")
+    println("R - RESET LEVEL")
+    println("Have fun!")
+    println("----------------------------------------------------------------")
+}
 fun main() {
+    welcome()
     val game = Game()
     game.renderBoard()
     println("Your input: ")
