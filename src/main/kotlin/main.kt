9ce06d610891bed937c0db5d04fc9210063f3272
Valko Martin <matinko.valko@gmail.com>
2023-11-11 14:40:28 +0100

    Revert "Added exception handling for file reading in main.kt"
    This reverts commit d37fabe01d63479154702dbdf982ee1465b54397.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,21 @@
 import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
-
 fun getLevel(path: String = "src/main/resources/level.txt"): MutableList<String> {
-    return try {
-        val fileLines: List<String> = File(path).readLines()
-        fileLines.toMutableList()
-    } catch (e: FileNotFoundException) {
-        System.err.println("File not found: ${e.message}")
-        mutableListOf()
-    } catch (e: IOException) {
-        System.err.println("Error reading file: ${e.message}")
-        mutableListOf()
+    val fileLines: List<String> = File(path).readLines()
+    val board: MutableList<String> = mutableListOf()
+    fileLines.forEach{
+        board.add(it)
     }
-
+    return board
 }
 
 val board: MutableList<String> = getLevel()
 fun main() {
-    if (board.isEmpty()) {
-        return
-    }
     val game = Game(board)
     game.renderBoard()
     println("Your input: ")
     var input = readln()
     while (input.isNotEmpty()) {
-        game.move(input.first())
+        game.move(input[0])
         if (game.isWon()) {
             println("CONGRATULATIONS, YOU WON!")
             return
